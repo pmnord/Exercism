7241ebeaa0ce017729e4618eb80d55ec3a41815f
Peter Nordman <peter.m.nordman@gmail.com>
2020-11-12 13:10:58 -0500

    Add TS exercise

---
+++ typescript/two-fer/two-fer.ts
@@ -0,0 +1,16 @@
+/**
+ * This stub is provided to make it straightforward to get started.
+ */
+
+export function twoFer(person:string='you'): string {
+  // ^                 ^              ^ this is called a return type; it's the type of the
+  // ^                 ^                value that is returned from this function
+  // ^                 ^
+  // ^                 parameters go here
+  // ^
+  // allows the tests to import this function and call it
+
+  // <-- Your code goes here. You may remove all the commentary in this file.
+
+  return `One for ${person}, one for me.`
+}
